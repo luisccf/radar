b653de1e4b197e013d0e6c4cfc1cb21ccb068248
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2016-09-28 21:01:48 -0300

    Prática introdução ao desenvolvimento Web utilizando Netbeans.

---
+++ pratica-introducao-web-netbeans/HelloWeb/src/main/java/ManagedBeans/LoginManagedBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBeans;
+
+import DAO.UsuarioDAO;
+import entity.Usuario;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+   
+  @ManagedBean(name = "LoginMB")
+  @ViewScoped
+  public class LoginManagedBean {
+   
+        private UsuarioDAO usuarioDAO = new UsuarioDAO();
+        private Usuario usuario = new Usuario();
+        
+        public String envia() {
+              
+              usuario = usuarioDAO.getUsuario(usuario.getNomeUsuario(), usuario.getSenha());
+              if (usuario == null) {
+                    usuario = new Usuario();
+                    FacesContext.getCurrentInstance().addMessage(
+                               null,
+                               new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!",
+                                           "Erro no Login!"));
+                    return null;
+              } else {
+                    return "/main";
+              }
+              
+              
+        }
+   
+        public Usuario getUsuario() {
+              return usuario;
+        }
+   
+        public void setUsuario(Usuario usuario) {
+              this.usuario = usuario;
+        }
+  }
