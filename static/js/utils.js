5b83bb2fb83e89bb254ba39db2b739828a39bca9
luisccf <luisccf@me.com>
2016-10-02 18:05:35 -0300

    Adiciona página de visualização de ocorrências

---
+++ static/js/utils.js
@@ -0,0 +1,29 @@
+var getMonth = function(month) {
+    switch (month) {
+        case 0:
+            return 'janeiro';
+        case 1:
+            return 'fevereiro';
+        case 2:
+            return 'março';
+        case 3:
+            return 'abril';
+        case 4:
+            return 'maio';
+        case 5:
+            return 'junho';
+        case 6:
+            return 'julho';
+        case 7:
+            return 'agosto';
+        case 8:
+            return 'setembro';
+        case 9:
+            return 'outubro';
+        case 10:
+            return 'novembro';
+        case 11:
+        default:
+            return 'dezembro';
+    }
+}
