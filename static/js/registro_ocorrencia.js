5097c163058907e75ab288885646690f505231a8
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-10-03 19:23:26 -0300

    Registro de Ocorrência: Validação das entradas

--- static/js/registro_ocorrencia.js
+++ static/js/registro_ocorrencia.js
@@ -3,13 +3,40 @@ document.getElementById("submit-button").addEventListener('click',function (evt)
   evt.preventDefault();
 
   var local = document.getElementById("local").value;
+  var horario = document.getElementById("horario").value;
+  var data = document.getElementById("data").value;
+
+  var localDiv = document.getElementById("local-div");
+  var localError = document.getElementById("local-error");
+  var horarioDiv = document.getElementById("horario-div");
+  var horarioError = document.getElementById("horario-error");
+  var dataDiv = document.getElementById("data-div");
+  var dataError = document.getElementById("data-error");
 
   if(local == "")
   {
-    var localDiv = document.getElementById("local-div");
-    var localError = document.getElementById("local-error");
+    localDiv.className = "form-group row has-error has-feedback";
+    localError.className = "glyphicon glyphicon-remove form-control-feedback";
+  } else {
+    localDiv.className = "form-group row has-success has-feedback";
+    localError.className = "glyphicon glyphicon-ok form-control-feedback";
+  }
 
-    localDiv.className += " has-error has-feedback";
-    localError.style.visibility = "visible";
+  if(horario == "")
+  {
+    horarioDiv.className += " has-error has-feedback";
+    horarioError.className = "glyphicon glyphicon-remove form-control-feedback";
+  } else {
+    horarioDiv.className = "form-group row has-success has-feedback";
+    horarioError.className = "glyphicon glyphicon-ok form-control-feedback";
+  }
+
+  if(data == "")
+  {
+    dataDiv.className += " has-error has-feedback";
+    dataError.className = "glyphicon glyphicon-remove form-control-feedback";
+  } else {
+    dataDiv.className = "form-group row has-success has-feedback";
+    dataError.className = "glyphicon glyphicon-ok form-control-feedback";
   }
 });
