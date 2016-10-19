c0e188f83830d788c68c8474cd97bcfa6aab89d1
luisccf <luisccf@me.com>
2016-10-19 00:27:13 -0200

    Implementa busca de ocorrências existentes via chamada assíncrona

--- radar-backend/src/main/webapp/js/incidents.js
+++ radar-backend/src/main/webapp/js/incidents.js
@@ -1,157 +1,127 @@
 var map,
-    markers = [];
-
-var incidents = [
-    {
-        'id': 1,
-        'username': 'luis',
-        'longitude': -42.9133,
-        'latitude': -19.9167,
-        'date': new Date(),
-        'description': 'Lorem ipsum',
-        'num_criminals': 2,
-        'armed': 1,
-        'victims_transportation': 'a pé',
-        'criminals_transportation': 'a pé',
-        'violence': 0,
-        'num_victims': 1,
-        'police_report': 0,
-        'objects_taken': [
-        ]
-    },
-    {
-        'id': 2,
-        'username': 'lccf',
-        'longitude': -43.9333,
-        'latitude': -19.9167,
-        'date': new Date(),
-        'description': 'Lorem ipsum',
-        'num_criminals': 3,
-        'armed': 1,
-        'victims_transportation': 'de carro',
-        'criminals_transportation': 'a pé',
-        'violence': 0,
-        'num_victims': 2,
-        'police_report': 0,
-        'objects_taken': [
-            'celular', 'óculos de sol', 'caneta bic'
-        ]
-    }
-];
+    markers = [],
+    infoWindows = [],
+    incidents = [];
 
 var initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -19.9167, lng: -43.9333},
+        center: {lat: 34.0093, lng: -118.4974},
         zoom: 8,
         maxZoom: 12,
         streetViewControl: false
     });
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map.setCenter(pos);
-        });
-    }
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //         var pos = {
+    //             lat: position.coords.latitude,
+    //             lng: position.coords.longitude
+    //         };
+    //         map.setCenter(pos);
+    //     });
+    // }
+    getIncidents();
 }
 
 var getIncidents = function() {
     $.ajax({
-        url: '/path/to/file',
+        url: '/getincidents',
     })
     .done(function(data) {
-        loadMarkers(data.incidents);
+        incidents = data;
+        loadMarkers(data);
     })
-    .fail(function() {
-        console.log("error");
-    });     
+    .fail(function(e) {
+        console.log(e);
+    });
 }
 
 var loadMarkers = function(incidents) {
     for (var i = 0; i < incidents.length; i++) {
         var marker = new google.maps.Marker({
             position: {
-                lat: incidents[i].latitude, 
-                lng: incidents[i].longitude
+                lat: parseFloat(incidents[i].latitude), 
+                lng: parseFloat(incidents[i].longitude)
             },
-            map: map,
-            title: incidents[i].date.toString()
+            map: map
         });
         marker.setValues({id: incidents[i].id});
         markers.push(marker);
+        createInfoWindow(incidents[i], marker);
         marker.addListener('click', function() {
             for (var j = 0; j < incidents.length; j++) {
                 if (incidents[j].id == this.get('id')) {
-                    loadInfoWindow(incidents[j]);
-                    break;
+                    for (var k = 0; k < infoWindows.length; k++) {
+                        if (this.get('id') == infoWindows[k].get('id')) {
+                            infoWindows[k].open(map, this);
+                        } else {
+                            infoWindows[k].close();
+                        }
+                    }
                 }
             }
         });
     }
 }
 
-var loadInfoWindow = function(incident) {
+var createInfoWindow = function(incident, marker) {
     var num_criminals = '',
         num_victims = '',
-        victims_transportation = '',
-        criminals_transportation = '',
-        objects_taken = '',
+        victims_transport = '',
+        criminals_transport = '',
+        objects_taken = incident.objects_taken ? incident.objects_taken : '';
         violence = incident.violence == 1 ? 'Houve violência' : 'Não houve violência',
         description = incident.description;
     if (incident.num_criminals > 1) {
         num_criminals += incident.num_criminals + ' assaltantes';
-        criminals_transportation = 'Os assaltantes estavam ' + incident.criminals_transportation;
+        criminals_transport = 'Os assaltantes estavam ' + incident.criminals_transport.name;
     } else {
         num_criminals += '' + incident.num_criminals + ' assaltante';
-        criminals_transportation = 'O assaltante estava ' + incident.criminals_transportation;
+        criminals_transport = 'O assaltante estava ' + incident.criminals_transport.name;
     }
     if (incident.num_victims > 1) {
         num_victims += '' + incident.num_victims + ' vítimas';
-        victims_transportation = 'As vítimas estavam ' + incident.victims_transportation;
+        victims_transport = 'As vítimas estavam ' + incident.victims_transport.name;
     } else {
         num_victims += incident.num_victims + ' vítima';
-        victims_transportation = 'A vítima estava ' + incident.victims_transportation;
+        victims_transport = 'A vítima estava ' + incident.victims_transport.name;
     }
-    var num_objects_taken = incident.objects_taken.length;
-    if (num_objects_taken) {
-        for (var i = 0; i < num_objects_taken - 1; i++) {
-            objects_taken += incident.objects_taken[i] + ', ';
-        }
-        objects_taken += incident.objects_taken[num_objects_taken - 1];
-    } else {
-        objects_taken = 'Nenhum!';
-    }
-    var footer = incident.username + ', ' 
-        + incident.date.getDate()
-        + ' de ' + getMonth(incident.date.getMonth())
-        + ' de ' + incident.date.getFullYear();
-
+    var date = new Date(incident.date)
+    var footer = incident.user.username + ', ' 
+        + date.getDate()
+        + ' de ' + getMonth(date.getMonth())
+        + ' de ' + date.getFullYear();
     var template = $('#info-window-template').html();
-    Mustache.parse(template);
-    var rendered = Mustache.render(
-        template, 
-        {
-            num_criminals: num_criminals,
-            num_victims: num_victims,
-            criminals_transportation: criminals_transportation,
-            victims_transportation, victims_transportation,
-            violence: violence,
-            description: description,
-            objects_taken: objects_taken,
-            footer: footer
+    var location = {lat: marker.position.lat(), lng: marker.position.lng()};
+    var formatted_address = [location.lat, location.lng].join(', ');
+    var geocoder = new google.maps.Geocoder;
+    exec = true;
+    geocoder.geocode({'location': location}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                formatted_address = results[1].formatted_address;
+                Mustache.parse(template);
+                var rendered = Mustache.render(
+                    template, 
+                    {
+                        formatted_address: formatted_address,
+                        num_criminals: num_criminals ? num_criminals : '',
+                        num_victims: num_victims ? num_victims : '',
+                        criminals_transport: criminals_transport ? criminals_transport : '',
+                        victims_transport: victims_transport ? victims_transport : '',
+                        violence: violence ? violence : '',
+                        description: description ? description : '',
+                        objects_taken: objects_taken ? objects_taken : '',
+                        footer: footer
+                    }
+                );
+                $('#info-window-body').html(rendered);
+                var infowindow = new google.maps.InfoWindow({
+                    content: $('#info-window-body').html()
+                });
+                infowindow.setValues({id: marker.get('id')});
+                infoWindows.push(infowindow);
+            }
         }
-    );
-    $('#info-window-body').html(rendered);
-    $('#info-window').modal();
+    });
 }
-
-$(function() {
-    initMap();
-    setTimeout(function() {
-        $('#login-window').modal();
-        loadMarkers(incidents);
-    }, 3000);
-});
