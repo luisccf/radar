997cc53a712e6be0604ce41fa05c606de71a45b6
luisccf <luisccf@me.com>
2016-10-20 12:27:00 -0200

    Corrige visualização de detalhes de ocorrência ao clicar em um marcador

--- radar-backend/src/main/webapp/js/incidents.js
+++ radar-backend/src/main/webapp/js/incidents.js
@@ -1,6 +1,6 @@
 var map,
     markers = [],
-    infoWindows = [],
+    infowindows = [],
     incidents = [];
 
 var initMap = function() {
@@ -11,60 +11,52 @@ var initMap = function() {
         streetViewControl: false
     });
 
-    // if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(function(position) {
-    //         var pos = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         };
-    //         map.setCenter(pos);
-    //     });
-    // }
+    // Loads map on user's current location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos);
+        });
+    }
     getIncidents();
 }
 
 var getIncidents = function() {
     $.ajax({
         url: '/getincidents',
-    })
-    .done(function(data) {
-        incidents = data;
-        loadMarkers(data);
-    })
-    .fail(function(e) {
-        console.log(e);
+        success: function(items) {
+            // Gets incidents and creates their markers and infowindows
+            $.each(items, function(i, item) {
+                incidents[item.id] = item;
+                var marker = createMarker(item);
+                markers[item.id] = marker
+                infowindows[item.id] = createInfowindow(item, marker);
+                marker.addListener('click', function() {
+                    infowindows[item.id].open(map, marker);
+                });
+            });
+        },
+        error: function(e) {
+            console.log(e);
+        }
     });
 }
 
-var loadMarkers = function(incidents) {
-    for (var i = 0; i < incidents.length; i++) {
-        var marker = new google.maps.Marker({
-            position: {
-                lat: parseFloat(incidents[i].latitude), 
-                lng: parseFloat(incidents[i].longitude)
-            },
-            map: map
-        });
-        marker.setValues({id: incidents[i].id});
-        markers.push(marker);
-        createInfoWindow(incidents[i], marker);
-        marker.addListener('click', function() {
-            for (var j = 0; j < incidents.length; j++) {
-                if (incidents[j].id == this.get('id')) {
-                    for (var k = 0; k < infoWindows.length; k++) {
-                        if (this.get('id') == infoWindows[k].get('id')) {
-                            infoWindows[k].open(map, this);
-                        } else {
-                            infoWindows[k].close();
-                        }
-                    }
-                }
-            }
-        });
-    }
+var createMarker = function(incident) {
+    var marker = new google.maps.Marker({
+        position: {
+            lat: incident.latitude, 
+            lng: incident.longitude
+        },
+        map: map
+    });
+    return marker;
 }
 
-var createInfoWindow = function(incident, marker) {
+var createInfowindow = function(incident, marker) {
     var num_criminals = '',
         num_victims = '',
         victims_transport = '',
@@ -97,35 +89,33 @@ var createInfoWindow = function(incident, marker) {
         $('<p>' + description + '</p>').appendTo(blockquote);
         $('<footer>' + footer + '</footer>').appendTo(blockquote);
     }
+    // Generates infowindow content using a template
     var template = $('#info-window-template').html();
-    var location = {lat: marker.position.lat(), lng: marker.position.lng()};
-    var formatted_address = [location.lat, location.lng].join(', ');
-    var geocoder = new google.maps.Geocoder;
-    geocoder.geocode({'location': location}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-            if (results[1]) {
-                formatted_address = results[1].formatted_address;
-                Mustache.parse(template);
-                var rendered = Mustache.render(
-                    template, 
-                    {
-                        formatted_address: formatted_address,
-                        num_criminals: num_criminals ? num_criminals : '',
-                        num_victims: num_victims ? num_victims : '',
-                        criminals_transport: criminals_transport ? criminals_transport : '',
-                        victims_transport: victims_transport ? victims_transport : '',
-                        violence: violence ? violence : '',
-                        blockquote: blockquote.prop('outerHTML'),
-                        objects_taken: objects_taken ? objects_taken : '',
-                    }
-                );
-                $('#info-window-body').html(rendered);
-                var infowindow = new google.maps.InfoWindow({
-                    content: $('#info-window-body').html()
-                });
-                infowindow.setValues({id: marker.get('id')});
-                infoWindows.push(infowindow);
-            }
+    // var location = {lat: marker.position.lat(), lng: marker.position.lng()};
+    // var formatted_address = [location.lat, location.lng].join(', ');
+    // var geocoder = new google.maps.Geocoder;
+    // geocoder.geocode({'location': location}, function(results, status) {
+    //     if (status === google.maps.GeocoderStatus.OK) {
+    //         if (results[1]) {
+    // formatted_address = results[1].formatted_address;
+    Mustache.parse(template);
+    var rendered = Mustache.render(
+        template, 
+        {
+            // formatted_address: formatted_address,
+            num_criminals: num_criminals ? num_criminals : '',
+            num_victims: num_victims ? num_victims : '',
+            criminals_transport: criminals_transport ? criminals_transport : '',
+            victims_transport: victims_transport ? victims_transport : '',
+            violence: violence ? violence : '',
+            blockquote: blockquote.prop('outerHTML'),
+            objects_taken: objects_taken ? objects_taken : '',
         }
+    );
+    $('#info-window-body').html(rendered);
+    return new google.maps.InfoWindow({
+        content: $('#info-window-body').html()
     });
+        //     } else console.log(status);
+        // } else console.log(status);
 }
