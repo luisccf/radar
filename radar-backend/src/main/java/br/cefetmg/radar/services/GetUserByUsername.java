7872f9a5b5f0a590cfa1119e47be6abc52d9d3e5
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2016-10-25 15:26:01 -0200

    Cria servlet GetUserByUsername.

---
+++ radar-backend/src/main/java/br/cefetmg/radar/services/GetUserByUsername.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.radar.services;
+
+import br.cefetmg.radar.dao.UserDAO;
+import br.cefetmg.radar.entity.Incident;
+import br.cefetmg.radar.entity.User;
+import br.cefetmg.radar.message.Result;
+import br.cefetmg.radar.util.typeadapter.HibernateProxyTypeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author rafae_000
+ */
+@WebServlet(name = "GetUserByUsername", urlPatterns = {"/getuserbyusername"})
+public class GetUserByUsername extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        GsonBuilder b = new GsonBuilder();
+        
+        b.registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY);
+        
+        Gson gson = b.create();
+
+        response.setContentType("text/json;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        
+        try{
+            UserDAO userDAO = new UserDAO();
+            
+            String username = request.getParameter("username");
+            
+            User user = userDAO.getByUsername(username);
+            
+            if (user != null){
+                
+                if(user.getColor() != null){
+                    user.getColor().setUsers(null);
+                }
+
+                if(user.getGender() != null){
+                    user.getGender().setUsers(null);
+                }            
+
+                for(Incident i : user.getIncidents()){
+                    i.setUser(null);
+                }
+            
+                out.println(gson.toJson(user));
+            } else {
+                out.println(gson.toJson(new Result(Result.USER_DOESNT_EXISTS)));
+                response.setStatus(404);
+            }
+            
+        } catch (Exception ex) {
+            out.println(gson.toJson(new Result(Result.ERRO, ex.getMessage())));
+            ex.printStackTrace();
+            response.setStatus(500);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
